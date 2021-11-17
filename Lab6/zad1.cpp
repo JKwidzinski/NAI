849e56871834d7eecec235507b465d57e7fa8e79
JKwidzinski <56433695+JKwidzinski@users.noreply.github.com>
2021-11-17 12:05:29 +0100

    Add files via upload

---
+++ Lab6/zad1.cpp
@@ -0,0 +1,23 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char **argv)
+{
+	Mat src=imread("image1.jpg");
+	namedWindow("input", WINDOW_AUTOSIZE);
+	while (true)
+	{
+		imshow("input", src);
+        Mat dst;
+        flip(src, dst, 1);
+		imshow("output", dst);
+		if (waitKey(1) == 81)
+			break;
+	}
+	return 0;
+}
